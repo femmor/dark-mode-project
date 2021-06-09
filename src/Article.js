0ec08f37b804e65a33899fbe6158205787dcca32
Emmanuel Egomson <fegomson@gmail.com>
2021-06-09 11:16:21 -0500

    Setup moment.js

--- src/Article.js
+++ src/Article.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import moment from 'moment'
 const Article = ({ title, snippet, date, length }) => {
+  const newDate = moment(date).format('dddd Do MMMM, YYYY')
+
   return (
     <article className='post'>
       <h2>{title}</h2>
       <div className="post-info">
-        <span>posted {date.toLocaleString()}</span>
-        <span> - {length} min read</span>
+        <span>{newDate}</span>
+        <span>- {length} min read</span>
       </div>
       <p>{snippet}</p>
     </article>
